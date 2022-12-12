e8a18ee6464de573b7915febc93895dcdc0a0100
AlexGrohmann <alex.grohmann@googlemail.com>
2022-12-12 21:27:15 +0100

    remove jquery

--- ChromeRuler/background.js
+++ ChromeRuler/background.js
@@ -1,33 +1,30 @@
-
-var Tabclicked= false;
+var Tabclicked = false;
 disableBrowserAction();
 
-function disableBrowserAction(tabId){
- 
-    chrome.tabs.executeScript(tabId, { file: "jquery.js" }, function() {
-    chrome.tabs.executeScript(tabId, { file: "CloseRuler.js" });
-	});
+function disableBrowserAction(tabId) {
+  chrome.tabs.executeScript(tabId, { file: "CloseRuler.js" }, function () {
+    console.log("ruler disabled");
+  });
 }
 
-function enableBrowserAction(tabId){
-    
-    chrome.tabs.executeScript(tabId, { file: "jquery.js" }, function() {
-    chrome.tabs.executeScript(tabId, { file: "Ruler.js" });
-	});
+function enableBrowserAction(tabId) {
+  chrome.tabs.executeScript(tabId, { file: "Ruler.js" }, function () {
+    console.log("Ruler active");
+  });
 }
 
-function updateState(){
-    if(Tabclicked==false){
-        Tabclicked=true;
-        enableBrowserAction();
-    }else{
-        Tabclicked=false;
-        disableBrowserAction();
-    }
+function updateState() {
+  if (Tabclicked == false) {
+    Tabclicked = true;
+    enableBrowserAction();
+  } else {
+    Tabclicked = false;
+    disableBrowserAction();
+  }
 }
 
 //chrome.pageAction.onClicked.addListener(updateState);
 
-chrome.browserAction.onClicked.addListener(function(tabId) {
-	updateState(tabId);
+chrome.browserAction.onClicked.addListener(function (tabId) {
+  updateState(tabId);
 });
