f423bad25535321b715da0b6f21a2ca4703a9004
AlexGrohmann <alex.grohmann@googlemail.com>
2024-05-01 16:25:53 +0200

    refactor code with chat gpt

--- draw-ruler/Ruler.js
+++ draw-ruler/Ruler.js
@@ -1,10 +1,12 @@
-//Adding events
 document.body.style.cursor = "crosshair";
-document.addEventListener("mousedown", Mousedownfunction);
-document.addEventListener("mouseup", Mouseupfunction);
-document.addEventListener("mousemove", Mousemovefunction);
 
-boxcss =
+// Event Listeners
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mouseup", handleMouseUp);
+document.addEventListener("mousemove", handleMouseMove);
+
+// Constants for CSS styles
+const boxCss =
   "background-color:#2b574b !important;" +
   "position:fixed !important;" +
   "width:180px !important;" +
@@ -13,7 +15,7 @@ boxcss =
   "z-index:10000000000000 !important;" +
   "display:block !important;";
 
-labelcss =
+const labelCss =
   "left: 5%;" +
   "color: white !important;" +
   "padding-top: 4px !important;" +
@@ -24,32 +26,72 @@ labelcss =
   "z-index:10000000000000 !important;" +
   "display:block !important;";
 
-CreateDisplayNodes("Top", "top:0px;left:40%;");
-CreateDisplayNodes("Bottom", "bottom:0px;left:40%;");
-CreateDisplayNodes("Left", "left:0px;top:40%;");
-CreateDisplayNodes("Right", "right:0px;top:40%;");
-CreateDisplayNodes(
-  "Width",
-  "right:0px;top:10px;background-color:#2b574b !important;"
-);
-CreateDisplayNodes(
-  "Height",
-  "right:0px;top:50px;background-color:#2b574b !important;"
-);
+// Event Handlers
+let clicked = false;
+let x = 0,
+  y = 0;
 
-function Mousedownfunction(e) {
+function handleMouseDown(e) {
   clicked = true;
   x = e.pageX;
   y = e.pageY;
   clearLines();
-  UpdateSpecs(x, y, x, y);
+  updateSpecs(x, y, x, y);
+  drawAxisLines(x, y);
+}
+
+function handleMouseUp() {
+  clicked = false;
+}
+
+function handleMouseMove(e) {
+  if (clicked) {
+    clearLines();
+    const x2 = e.pageX,
+      y2 = e.pageY;
+    updateSpecs(x, y, x2, y2);
+    drawAxisLines(x, y);
+    drawAxisLines(x2, y2, "2");
+    drawRectangle(x, y, x2, y2);
+  }
+}
+
+// Utility functions
+function clearLines() {
+  const lines = document.querySelectorAll(
+    "[id^='ChromeRulerhorizontal'], [id^='ChromeRulervertical'], #ChromeRulerSelectionRectangle"
+  );
+  lines.forEach((line) => line.remove());
+}
+
+function createLine(x1, y1, x2, y2, id) {
+  const a = x1 - x2,
+    b = y1 - y2,
+    c = Math.sqrt(a * a + b * b);
+  const sx = (x1 + x2) / 2,
+    sy = (y1 + y2) / 2;
+  const x = sx - c / 2,
+    y = sy;
+  const alpha = Math.PI - Math.atan2(-b, a);
+  return createLineElement(x, y, c, alpha, id);
+}
+
+function createLineElement(x, y, length, angle, id) {
+  const line = document.createElement("div");
+  line.id = `ChromeRuler${id}`;
+  const styles = `border-Top: thin dashed #e04300; z-index:10000000000000 !important; display:block !important; width: ${length}px; height: 0px; -moz-transform: rotate(${angle}rad); -webkit-transform: rotate(${angle}rad); -o-transform: rotate(${angle}rad); -ms-transform: rotate(${angle}rad); position: absolute; top: ${y}px; left: ${x}px;`;
+  line.style.cssText = styles;
+  return line;
+}
+
+function drawAxisLines(x, y, axis = "1") {
   document.body.appendChild(
     createLine(
       x,
       0,
       x,
       document.body.getBoundingClientRect().height,
-      "vertical1"
+      `vertical${axis}`
     )
   );
   document.body.appendChild(
@@ -58,181 +100,68 @@ function Mousedownfunction(e) {
       y,
       document.body.getBoundingClientRect().width,
       y,
-      "horizontal1"
+      `horizontal${axis}`
     )
   );
 }
 
-function Mouseupfunction(e) {
-  clicked = false;
+function drawRectangle(x1, y1, x2, y2) {
+  const rect = document.createElement("div");
+  const style = `left:${Math.min(x1, x2)}px;top:${Math.min(
+    y1,
+    y2
+  )}px;position:absolute;z-index:10000000000000 !important;opacity:0.5;display:block !important;width:${Math.abs(
+    x2 - x1
+  )}px;height:${Math.abs(y2 - y1)}px;background-color:#e04300`;
+  rect.style.cssText = style;
+  rect.id = "ChromeRulerSelectionRectangle";
+  document.body.appendChild(rect);
 }
 
-function Mousemovefunction(e) {
-  if (clicked == true) {
-    //dragging
-    clearLines();
-    let x2 = e.pageX,
-      y2 = e.pageY;
-    UpdateSpecs(x, y, x2, y2);
-    //Axis1
-    document.body.appendChild(
-      createLine(
-        x,
-        0,
-        x,
-        document.body.getBoundingClientRect().height,
-        "vertical1"
-      )
-    );
-    document.body.appendChild(
-      createLine(
-        0,
-        y,
-        document.body.getBoundingClientRect().width,
-        y,
-        "horizontal1"
-      )
-    );
+function createDisplayNodes(name, position) {
+  const node = document.createElement("div");
+  const nodeStyle = boxCss + position;
+  node.style.cssText = nodeStyle;
+  node.id = `ChromeRuler${name}DisplayNode`;
 
-    //Axis2
-    document.body.appendChild(
-      createLine(
-        x2,
-        0,
-        x2,
-        document.body.getBoundingClientRect().height,
-        "vertical2"
-      )
-    );
-    document.body.appendChild(
-      createLine(
-        0,
-        y2,
-        document.body.getBoundingClientRect().width,
-        y2,
-        "horizontal2"
-      )
-    );
+  const textNode = document.createElement("label");
+  textNode.style.cssText = labelCss;
+  textNode.id = `ChromeRuler${name}`;
+  textNode.textContent = name;
 
-    DrawRectangle(x, y, x2, y2);
-  }
-}
-
-//clearing lines
-function clearLines() {
-  if (document.getElementById("ChromeRulerhorizontal1"))
-    document.getElementById("ChromeRulerhorizontal1").remove();
-
-  if (document.getElementById("ChromeRulervertical1"))
-    document.getElementById("ChromeRulervertical1").remove();
-
-  if (document.getElementById("ChromeRulerhorizontal2"))
-    document.getElementById("ChromeRulerhorizontal2").remove();
-
-  if (document.getElementById("ChromeRulervertical2"))
-    document.getElementById("ChromeRulervertical2").remove();
-
-  if (document.getElementById("ChromeRulerSelectionRectangle"))
-    document.getElementById("ChromeRulerSelectionRectangle").remove();
-}
-
-//drawing lines
-function createLineElement(x, y, length, angle, id) {
-  let line = document.createElement("div");
-  line.setAttribute("id", "ChromeRuler" + id);
-
-  let styles =
-    "border-Top: thin dashed #e04300; z-index:10000000000000 !important; display:block !important;" +
-    "width: " +
-    length +
-    "px; " +
-    "height: 0px; " +
-    "-moz-transform: rotate(" +
-    angle +
-    "rad); " +
-    "-webkit-transform: rotate(" +
-    angle +
-    "rad); " +
-    "-o-transform: rotate(" +
-    angle +
-    "rad); " +
-    "-ms-transform: rotate(" +
-    angle +
-    "rad); " +
-    "position: absolute; " +
-    "top: " +
-    y +
-    "px; " +
-    "left: " +
-    x +
-    "px; ";
-  line.setAttribute("style", styles);
-  return line;
-}
-
-function createLine(x1, y1, x2, y2, id) {
-  let a = x1 - x2,
-    b = y1 - y2,
-    c = Math.sqrt(a * a + b * b);
-
-  let sx = (x1 + x2) / 2,
-    sy = (y1 + y2) / 2;
-
-  let x = sx - c / 2,
-    y = sy;
-
-  let alpha = Math.PI - Math.atan2(-b, a);
-
-  return createLineElement(x, y, c, alpha, id);
-}
-
-function DrawRectangle(x1, y1, x2, y2) {
-  let rec = document.createElement("div");
-  rec.setAttribute("id", "ChromeRulerSelectionRectangle");
-  let style =
-    "left:" +
-    Math.min(x1, x2) +
-    "px;" +
-    "top:" +
-    Math.min(y1, y2) +
-    "px;" +
-    "position:absolute;z-index:10000000000000 !important;opacity:0.5;display:block !important;" +
-    "width:" +
-    Math.abs(x2 - x1) +
-    "px;" +
-    "height:" +
-    Math.abs(y2 - y1) +
-    "px;" +
-    "background-color:#e04300";
-  rec.setAttribute("style", style);
-  document.body.appendChild(rec);
-}
-
-function CreateDisplayNodes(name, position) {
-  let node = document.createElement("div");
-  let nodestyle = boxcss + position;
-  node.setAttribute("style", nodestyle);
-  node.setAttribute("id", "ChromeRuler" + name + "DisplayNode");
-
-  let textnode = document.createElement("label");
-  textnode.setAttribute("style", labelcss);
-  textnode.setAttribute("id", "ChromeRuler" + name);
-  textnode.innerHTML = name;
-
-  node.appendChild(textnode);
-
+  node.appendChild(textNode);
   document.body.appendChild(node);
 }
 
-function UpdateSpecs(x1, y1, x2, y2) {
-  document.getElementById("ChromeRulerTop").innerHTML = "Top : " + y1 + " px";
-  document.getElementById("ChromeRulerBottom").innerHTML =
-    "Bottom :  " + Math.abs(pageHeight - y2) + " px";
-  document.getElementById("ChromeRulerLeft").innerHTML = "Left : " + x1 + " px";
-  document.getElementById("ChromeRulerRight").innerHTML =
-    "Right :  " + Math.abs(pageWidth - x2) + " px";
-  document.getElementById("ChromeRulerWidth").innerHTML =
-    "Width : " + Math.abs(x2 - x1) + " px";
-  document.getElementById("ChromeRulerHeight").innerHTML =
-    "Height : " + Math.abs(y2 - y1) + " px";
+function updateSpecs(x1, y1, x2, y2) {
+  const pageWidth = document.body.getBoundingClientRect().width;
+  const pageHeight = document.body.getBoundingClientRect().height;
+  document.getElementById("ChromeRulerTop").textContent = `Top : ${y1}px`;
+  document.getElementById(
+    "ChromeRulerBottom"
+  ).textContent = `Bottom : ${Math.abs(pageHeight - y2)}px`;
+  document.getElementById("ChromeRulerLeft").textContent = `Left : ${x1}px`;
+  document.getElementById("ChromeRulerRight").textContent = `Right : ${Math.abs(
+    pageWidth - x2
+  )}px`;
+  document.getElementById("ChromeRulerWidth").textContent = `Width : ${Math.abs(
+    x2 - x1
+  )}px`;
+  document.getElementById(
+    "ChromeRulerHeight"
+  ).textContent = `Height : ${Math.abs(y2 - y1)}px`;
 }
+
+// Creating display nodes
+createDisplayNodes("Top", "top:0px;left:40%;");
+createDisplayNodes("Bottom", "bottom:0px;left:40%;");
+createDisplayNodes("Left", "left:0px;top:40%;");
+createDisplayNodes("Right", "right:0px;top:40%;");
+createDisplayNodes(
+  "Width",
+  "right:0px;top:10px;background-color:#2b574b !important;"
+);
+createDisplayNodes(
+  "Height",
+  "right:0px;top:50px;background-color:#2b574b !important;"
+);
